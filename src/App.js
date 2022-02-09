01256243f57ac794e074ef8a800dc4bfd32ba86a
Parth Roy <parthroy@i2i-info.com>
2022-02-09 19:12:08 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -9,11 +9,60 @@ import ProductsDetails from "./pages/ProductsDetails";
 import NotFound from "./pages/NotFound";
 import Cart from "./pages/Cart";
 import Register from "./pages/Register/Register";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  FaLinkedin,
+  FaGithubSquare,
+  FaFacebookSquare,
+  FaTwitterSquare,
+  FaInstagramSquare,
+} from "react-icons/fa";
 function App() {
   return (
     <div className="App">
       <Routers>
+        <div className="i2i-header d-flex justify-content-between">
+          <a href="http://www.i2i-info.com/" className="text-white p-2">
+            Developed by : Idea to Implementation Infotech
+          </a>
+          <div className="follow align-self-center">
+            <a
+              href="https://github.com/i2i-info/i2i-Store.git"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaGithubSquare />
+            </a>
+            <a
+              href="https://www.linkedin.com/company/i2i-infotech"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaLinkedin />
+            </a>
+            <a
+              href="https://www.facebook.com/Idea.to.implementation.infotech"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaFacebookSquare />
+            </a>
+            <a
+              href="https://twitter.com/i2i_infotech"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaTwitterSquare />
+            </a>
+            <a
+              href="https://www.instagram.com/i2i_infotech"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaInstagramSquare />
+            </a>
+          </div>
+        </div>
         <Routes>
           <Route index path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
